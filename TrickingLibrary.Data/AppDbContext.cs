68e0e498111a26e1752486717a55b5a4135148d1
T0shik <antonwieslander@hotmail.com>
2020-07-08 00:18:11 +0100

    ep12 trick facts

--- TrickingLibrary.Data/AppDbContext.cs
+++ TrickingLibrary.Data/AppDbContext.cs
@@ -9,5 +9,30 @@ namespace TrickingLibrary.Data
 
         public DbSet<Trick> Tricks { get; set; }
         public DbSet<Submission> Submissions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<TrickRelationship> TrickRelationships { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<TrickCategory> TrickCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TrickCategory>()
+                        .HasKey(x => new {x.CategoryId, x.TrickId});
+
+            modelBuilder.Entity<TrickRelationship>()
+                        .HasKey(x => new {x.PrerequisiteId, x.ProgressionId});
+
+            modelBuilder.Entity<TrickRelationship>()
+                        .HasOne(x => x.Progression)
+                        .WithMany(x => x.Prerequisites)
+                        .HasForeignKey(x => x.ProgressionId);
+
+            modelBuilder.Entity<TrickRelationship>()
+                        .HasOne(x => x.Prerequisite)
+                        .WithMany(x => x.Progressions)
+                        .HasForeignKey(x => x.PrerequisiteId);
+        }
     }
 }
