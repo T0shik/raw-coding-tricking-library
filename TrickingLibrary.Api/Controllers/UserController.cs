b84c9cf35a71a7ea49371c734611e12c42f13fdd
T0shik <antonwieslander@hotmail.com>
2020-09-08 19:24:21 +0100

    ep34 - silent renew missed

---
+++ TrickingLibrary.Api/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("me")]
+        public IActionResult GetMe() => Ok();
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(string id) => Ok();
+    }
+}
---
+++ TrickingLibrary.Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace TrickingLibrary.Models
+{
+    public class User : BaseModel<string>
+    {
+        public string Name { get; set; }
+    }
+}
